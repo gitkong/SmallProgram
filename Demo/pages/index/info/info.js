0bba5ecc0c42846032f0c5b580d0d4311b1fe070
clarence <13751855378@163.com>
2016-12-20 13:05:46 +0800

    完成注册程序页面搭建

--- Demo/pages/index/info/info.js
+++ Demo/pages/index/info/info.js
@@ -12,31 +12,7 @@ Page({
       'https://mp.weixin.qq.com/debug/wxadoc/dev/image/tabbar.png?t=20161122',
     ]
   },
-  showJson:function(e){
-    var str = e.target.id
-    if (typeof str != 'string') {
-        str = str.stringify(json, undefined, 2);
-    }
-    str = str.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
-    this.setData({
-      json : str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    })
-    }
-    )
-  },
+  
   jump:function(){
     wx.showModal({
       title: '提示',
--- Demo/pages/index/registPages/registPages.js
+++ Demo/pages/index/registPages/registPages.js
@@ -1,6 +1,39 @@
 // pages/index/registPages/registPages.js
 Page({
-  data:{},
+  data:{
+    codes : [
+      '//index.js\nPage({\ndata: {\ntext: "This is page data."\n},\nonLoad: function(options) {\n// Do some initialize when page load.\n},\nonReady: function() {\n  // Do something when page ready.\n},\nonShow: function() {\n  // Do something when page show.\n},\nonHide: function() {\n  // Do something when page hide.\n},\nonUnload: function() {\n  // Do something when page close.\n},\nonPullDownRefresh: function() {\n  // Do something when pull down.\n},\nonReachBottom: function() {\n  // Do something when page reach bottom.\n},\n// Event handler.\nviewTap: function() {\n  this.setData({\n    text: "Set some data for updating view."\n  })\n},\ncustomData: {\n  hi: "MINA"\n}\n})',
+
+      '<view>{{text}}</view>\n<view>{{array[0].msg}}</view>',
+
+      'Page({\ndata: {\n  text: "init data",\n  array: [{msg: "1"}, {msg: "2"}]\n}\n})',
+
+      '<view bindtap="viewTap"> click me </view>',
+
+      'Page({\nviewTap: function() {\nconsole.log("view tap")\n}\n})',
+
+      '<!--index.wxml-->\n<view>{{text}}</view>\n<button bindtap="changeText"> Change normal data </button>\n<view>{{array[0].text}}</view>\n<button bindtap="changeItemInArray"> Change Array data </button>\n<view>{{object.text}}</view>\n<button bindtap="changeItemInObject"> Change Object data </button>\n<view>{{newField.text}}</view>\n<button bindtap="addNewField"> Add new data </button>',  
+
+      '//index.js\nPage({\n data: {\n   text: "init data",\n   array: [{text: "init data"}],\n   object: {\n     text: "init data"\n   }\n },\n changeText: function() {\n    // this.data.text = "changed data"  // bad, it can not work\n    this.setData({\n      text: "changed data"\n   })\n  },\n  changeItemInArray: function() {\n    // you can use this way to modify a danamic data path\n    this.setData({\n      "array[0].text":"changed data"\n    })\n  },\n  changeItemInObject: function(){\n    this.setData({\n      "object.text": "changed data"\n    });\n  },\n  addNewField: function() {\n    this.setData({\n      "newField.text": "new data"\n  })\n  }\n})',
+    ],
+    images : [
+      'https://mp.weixin.qq.com/debug/wxadoc/dev/image/mina-lifecycle.png?t=20161122',
+    ],
+
+    tranferText : '<navigator/>',
+  },
+
+  jump:function(){
+    wx.showModal({
+      title: '提示',
+      content: '敬请期待',
+       success: function(res) {
+        if (res.confirm) {
+          // console.log('用户点击确定')
+        }
+      }
+    })
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
