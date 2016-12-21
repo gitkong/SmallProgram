1f71aaf60a43420808c68679534754488f9e8ab0
clarence <13751855378@163.com>
2016-12-21 10:31:35 +0800

    完成条件渲染详情页面搭建

--- Demo/pages/index/view/WXML/conditionDye/conditionDye.js
+++ Demo/pages/index/view/WXML/conditionDye/conditionDye.js
@@ -1,6 +1,18 @@
 // pages/index/view/WXML/conditionDye/conditionDye.js
 Page({
-  data:{},
+  data:{
+    tranferText : '<block/>',
+    condition : 'condition',
+    codes : [
+      '<view wx:if="{{condition}}"> True </view>',
+
+      '<view wx:if="{{length > 5}}"> 1 </view>\n<view wx:elif="{{length > 2}}"> 2 </view>\n<view wx:else> 3 </view>',
+
+      '<block wx:if="{{true}}">\n  <view> view1 </view>\n  <view> view2 </view>\n</block>',
+
+      '',
+    ],
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
